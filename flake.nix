0a96d111ed71324a0bc4b0935329663cfa02a2b0
767mmy5p <89712398+767mmy5p@users.noreply.github.com>
2026-08-25 02:10:02 -0400

    Update hash for GitHub fetch in flake.nix

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
           owner = "FLSoz";
           repo = "terratech-steam-mod-loader";
           rev = "v${version}";
-          hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          hash = "sha256-qAsWYeNyo3qNiuE2jZ6aZCdQ4h7GH/bdPpHwqsi/2Ek=";
         };
 
         # Shared native libs for Electron runtime and native Node addons
